4f02337f1e1527f060c61836dbfcdcb1897731d0
Bobby Wang <wbo4958@gmail.com>
2016-11-20 14:10:15 +0800

    can work

--- app/src/main/java/cc/xiaokr/herecare/KnowledgeActivity.java
+++ app/src/main/java/cc/xiaokr/herecare/KnowledgeActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 
 public class KnowledgeActivity extends BaseActivity {
     private View mIvBackground;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_knowledge);
 
         mIvBackground = findViewById(R.id.activity_knowledge);
-        mIvBackground.setBackground(Utils.readBgDrawable(this, R.drawable.love_map));
+        mIvBackground.setBackground(Utils.readBgDrawable(this, R.drawable.bg_knowledge));
     }
 
     @Override
