d30133192a35e4bdacb6f60f33634e12505f3fcb
Bobby Wang <wbo4958@gmail.com>
2016-11-20 09:23:29 +0800

    first page can work

--- app/src/main/java/cc/xiaokr/herecare/WelcomeActivity.java
+++ app/src/main/java/cc/xiaokr/herecare/WelcomeActivity.java
@@ -1,27 +1,59 @@
 package cc.xiaokr.herecare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class WelcomeActivity extends BaseActivity implements View.OnClickListener {
 
-    private Button mBtTeaching, mBtMap, mBtTraining;
+    private ImageButton mBtTeaching, mBtMap, mBtTraining;
+    private RelativeLayout mWelcomeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
+        mWelcomeLayout = (RelativeLayout) findViewById(R.id.layout_welcome);
+        mWelcomeLayout.setBackground(readBgDrawable(this, R.drawable.cpr_first_page));
+//        mWelcomeLayout.setBackgroundDrawable(readBgDrawable(this, R.drawable.cpr_first_page));
 
-        mBtMap = (Button) findViewById(R.id.bt_love_map);
+        mBtMap = (ImageButton) findViewById(R.id.bt_love_map);
         mBtMap.setOnClickListener(this);
 
-        mBtTeaching = (Button) findViewById(R.id.bt_teaching_material);
+        mBtTeaching = (ImageButton) findViewById(R.id.bt_teaching_material);
         mBtTeaching.setOnClickListener(this);
 
-        mBtTraining = (Button) findViewById(R.id.bt_training);
+        mBtTraining = (ImageButton) findViewById(R.id.bt_training);
         mBtTraining.setOnClickListener(this);
+    }
+
+    private BitmapDrawable readBgDrawable(Context context, int resId) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        opt.inPurgeable = true;
+        opt.inInputShareable = true;
+        //获取资源图片
+        InputStream is = context.getResources().openRawResource(resId);
+        Bitmap bitmap = BitmapFactory.decodeStream(is, null, opt);
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new BitmapDrawable(context.getResources(), bitmap);
     }
 
     @Override
